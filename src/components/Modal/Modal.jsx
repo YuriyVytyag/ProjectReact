a94f027b5aae16c9e62fb35d08a308dc16b7b6de
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-02-20 15:12:37 +0200

    createrepo

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+
+const Modal = () => {
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+};
+
+export default Modal;
