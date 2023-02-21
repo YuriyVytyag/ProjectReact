d7f8ce0c3752eefe98d30249fca377dea8a0d4e7
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-02-21 15:59:35 +0200

    add burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,40 @@
 import Logo from 'components/Logo/Logo';
 import Navigation from 'components/Navigation/Navigation';
-import React from 'react';
+import UserInfo from 'components/UserInfo/UserInfo';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import s from '../Header/Header.module.css';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 function Header() {
+  // const token = .... необхідно додати!!!!!!!!
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleClick() {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <header className={s.header}>
       <Logo />
-      <Navigation />
+      <div className={s.Navigation}>
+        <Navigation />
+      </div>
+
+      <div className={s.UserInfo}>
+        <UserInfo />
+      </div>
+      <div className={s.burger}>
+        <button className={s.btn} onClick={handleClick}>
+          {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </button>
+        {isOpen && (
+          <div className={s.mobileMenu}>
+            <NavLink className={s.mobileLink}>Diary</NavLink>
+            <NavLink className={s.mobileLink}>Calculator</NavLink>
+          </div>
+        )}
+      </div>
     </header>
   );
 }
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,11 +6,13 @@ import s from '../Logo/Logo.module.css';
 
 function Logo() {
   return (
-    <>
+    <div className={s.logoWrapper}>
       <img className={s.logo_480} src={logo_480} alt="logo" />
       <img className={s.logo_768} src={logo_768} alt="logo" />
-      <img className={s.logo_1280} src={logo_1280} alt="logo" />
-    </>
+      <div className={s.logoLine}>
+        <img className={s.logo_1280} src={logo_1280} alt="logo" />
+      </div>
+    </div>
   );
 }
 
