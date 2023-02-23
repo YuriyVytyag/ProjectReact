0e5c763b8cc59f3a0ce8927d6c0484086729a668
s-hitsan <vasyliohloserhii@gmail.com>
2023-02-23 11:16:29 +0200

    registration fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router';
 
 import Login from './pages/Login';
 import Header from './components/Header/Header';
+import RegistrationPage from './pages/RegistrationPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Header />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<RegistrationPage />} />
 
     </Routes>
   );
