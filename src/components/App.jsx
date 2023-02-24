f11859f254cf16f1b257843d75b463ae0067e2d6
Olga Kaplunivska <kaplunovskaya.o@gmail.com>
2023-02-24 09:26:08 +0100

    fixed bag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import { Route, Routes } from 'react-router';
-import Header from './Header/Header';
-// import { FormLogin } from './LoginForm/index';
-import { RegistrationForm } from './RegistrationForm/index';
-// import Modal from './Modal/Modal';
+// import { Route, Routes } from 'react-router';
+// import Header from './Header/Header';
+// // import { FormLogin } from './LoginForm/index';
+// import { RegistrationForm } from './RegistrationForm/index';
+// // import Modal from './Modal/Modal';
 
-const App = () => {
-  return (
-  //  <FormLogin></FormLogin>
-   <RegistrationForm></RegistrationForm>
-  );
-};
+// const App = () => {
+//   return (
+//   //  <FormLogin></FormLogin>
+//    <RegistrationForm></RegistrationForm>
+//   );
+// };
 
-export default App;
+// export default App;
