758293835dcc6dbfdd0b08239049d83f2224d7c3
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-02-24 12:22:05 +0200

    Nav fixed

--- src/index.js
+++ src/index.js
@@ -10,15 +10,12 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
           <App />
-          </BrowserRouter>
-        </PersistGate>
-      </Provider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
-
-
-
