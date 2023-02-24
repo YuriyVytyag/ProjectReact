fee9ba78e90bde1f253e34532531f35355651c25
Yuruy Vytyaganets <yvytyawork@gmail.com>
2023-02-24 13:38:38 +0200

    Added calculator

--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -1,11 +1,22 @@
+import { RightSideBar } from 'components/RightSideBar/RightSideBar';
 import { DailyCaloriesForm } from 'components/Daily/DailyCaloriesForm';
+import styled from '@emotion/styled';
 
 const Calculator = () => {
   return (
-    <>
+    <StyledDiary>
       <DailyCaloriesForm />
-    </>
+      <RightSideBar />
+    </StyledDiary>
   );
 };
 
 export default Calculator;
+
+const StyledDiary = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media screen and (min-width: 1200px) {
+    flex-direction: row;
+  }
+`;
