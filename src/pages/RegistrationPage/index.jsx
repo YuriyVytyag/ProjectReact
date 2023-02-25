674725b7d9d1b2a0505865391d4d7abc3943eb34
Olga Kaplunivska <kaplunovskaya.o@gmail.com>
2023-02-25 12:21:19 +0100

    background

--- src/pages/RegistrationPage/index.jsx
+++ src/pages/RegistrationPage/index.jsx
@@ -1,8 +1,10 @@
+import { AuthBackground } from 'components/AuthBackground';
 import { RegistrationForm } from 'components/RegistrationForm';
 
 const RegistrationPage = () => {
   return (
     <>
+     <AuthBackground/>
       <RegistrationForm />
     </>
   );
